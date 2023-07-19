6b27f2d5eec6dcddfabd5f896932825bba16a337
JaSonic <112668485+JaSonic4PSHSMC@users.noreply.github.com>
2023-07-20 07:27:43 +0800

    rename beta to 0.1.1

--- khubdarkmode.user.js
+++ khubdarkmode.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         KHub Dark Mode
 // @namespace    http://tampermonkey.net/
-// @version      BetaRelease1
+// @version      0.1.1
 // @description  A dark mode script for KHub.
 // @author       JaSonic / Twitter: @therealjasonic / Discord: therealjasonic
 // @match        *://*.pshs.edu.ph/*
